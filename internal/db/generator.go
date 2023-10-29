e5aa482d0f5b5dec61f43b9dc05e4b4e77723047
Antony Ho <ntonyworkshop@gmail.com>
2023-10-29 18:38:59 +0100

    Add db generator boilerplate and unit test

--- internal/db/generator.go
+++ internal/db/generator.go
@@ -12,8 +12,49 @@ type Entry struct {
 	Katakana            bool
 	PunctuationMark     bool
 	MiscSymbol          bool
-	CangjieV3Code       string
-	CangjieV5Code       string
+	CangjieV3Radicals   string
+	CangjieV5Radicals   string
 	ShortCut            string
 	SortOrder           uint16
+}
+
+const (
+	CreateCharsTableQuery = `
+	CREATE TABLE characters (
+		idx INTEGER NOT NULL PRIMARY KEY ASC,
+		tc TEXT NOT NULL UNIQUE,
+		sc TEXT,
+		chinese INTEGER,
+		big5 INTEGER,
+		hkcsc INTEGER,
+		zhuyin INTEGER,
+		kanji INTEGER,
+		hiragana INTEGER,
+		katakana INTEGER,
+		punctuation INTEGER,
+		symbol INTEGER
+	);
+	`
+
+	CreateRadicalsTableQuery = `
+	CREATE TABLE radicals (
+		char_idx INTEGER NOT NULL,
+		version INTEGER NOT NULL,
+		radicals TEXT NOT NULL,
+		FOREIGN KEY(char_idx) REFERENCES characters(idx)
+	);
+	`
+
+	AddCharsQuery = `
+	INSERT INTO characters VALUES ();
+	`
+
+	AddRadicalsQuery = `
+	INSERT INTO radicals VALUES ();
+	`
+)
+
+// Generate SQLite3 database file from raw data
+func Generate(raw [][]string, dbFilePath string) error {
+	return nil
 }
--- internal/db/generator_test.go
+++ internal/db/generator_test.go
@@ -1,1 +1,36 @@
 package db_test
+
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/antonyho/go-cangjie/internal/data"
+	"github.com/antonyho/go-cangjie/internal/db"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerate(t *testing.T) {
+	cangjieTable := loadTestTableData(t)
+
+	tempDbFile := path.Join(t.TempDir(), "test.db")
+	err := db.Generate(cangjieTable, tempDbFile)
+	if !assert.NoError(t, err) {
+		assert.FailNow(t, "failed generating database", err)
+	}
+
+	// TODO - Verify the db
+}
+
+func loadTestTableData(t *testing.T) [][]string {
+	testTable, err := os.Open("testdata/table.txt")
+	if !assert.NoError(t, err) {
+		assert.FailNow(t, "failed loading test data", err)
+	}
+	cangjieTable, err := data.ReadTable(testTable)
+	if !assert.NoError(t, err) {
+		assert.FailNow(t, "failed parsing table data", err)
+	}
+
+	return cangjieTable
+}
