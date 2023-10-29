2a038d51edd52dcc236ecd56ee312c59374fe8ec
Antony Ho <ntonyworkshop@gmail.com>
2023-10-29 17:21:43 +0100

    Implementing reader for embed Cangjie table file

--- internal/data/reader.go
+++ internal/data/reader.go
@@ -1,22 +1,66 @@
 package data
 
 import (
-	"bytes"
-	_ "embed"
-	"encoding/csv"
+	"bufio"
+	"embed"
+	"errors"
+	"fmt"
+	"io/fs"
+	"strings"
+)
+
+// Errors on parsing raw data from embed data file
+var (
+	ErrMalformEntry = errors.New("data: malform entry")
+	ErrCommentLine  = errors.New("data: comment line")
+	ErrEmptyLine    = errors.New("data: empty line")
 )
 
 //go:embed assets/table.txt
-var builtinCangjieTable []byte
+var builtinCangjieTable embed.FS
 
-func ReadTable(cangjieTableContent []byte) ([][]string, error) {
-	reader := csv.NewReader(bytes.NewReader(cangjieTableContent))
-	reader.Comma = ' '
-	reader.Comment = '#'
-	reader.LazyQuotes = true
-	return reader.ReadAll()
+func ReadTable(cangjieTableContent fs.File) ([][]string, error) {
+	table := make([][]string, 0)
+	scanner := bufio.NewScanner(cangjieTableContent)
+	for lineNum := 0; scanner.Scan(); lineNum++ {
+		entry, err := readRaw(scanner.Text())
+		if err != nil {
+			switch err {
+			case ErrCommentLine, ErrEmptyLine:
+				continue
+			default:
+				return nil, fmt.Errorf("line %d: %w", lineNum, err)
+			}
+		}
+		table = append(table, entry)
+	}
+
+	return table, nil
 }
 
 func ReadBuiltinTable() ([][]string, error) {
-	return ReadTable(builtinCangjieTable)
+	file, err := builtinCangjieTable.Open("assets/table.txt")
+	if err != nil {
+		return nil, err
+	}
+	return ReadTable(file)
+}
+
+// Read the line of raw data from the embed Cangjie codes table.
+// Refer to the readme file in `assets/` directory for the data format.
+func readRaw(line string) ([]string, error) {
+	trimmedLine := strings.TrimSpace(line)
+	if len(trimmedLine) == 0 {
+		return nil, ErrEmptyLine
+	}
+	if trimmedLine[0] == '#' {
+		return nil, ErrCommentLine
+	}
+
+	fields := strings.Split(line, " ")
+	if len(fields) < 15 {
+		return nil, ErrMalformEntry
+	}
+
+	return fields, nil
 }
--- internal/data/reader_test.go
+++ internal/data/reader_test.go
@@ -4,12 +4,14 @@ import (
 	"os"
 	"testing"
 
-	"github.com/antonyho/go-cangjie/data"
+	"github.com/antonyho/go-cangjie/internal/data"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestReadTable(t *testing.T) {
-	testTable, err := os.ReadFile("testdata/table.txt")
+	const expectedNumOfEntry = 5
+
+	testTable, err := os.Open("testdata/table.txt")
 	if !assert.NoError(t, err) {
 		assert.FailNow(t, "failed loading test data", err)
 	}
@@ -17,13 +19,19 @@ func TestReadTable(t *testing.T) {
 	if !assert.NoError(t, err) {
 		assert.FailNow(t, "failed parsing table data", err)
 	}
-	assert.Len(t, cangjieTable, 5)
+	assert.Len(t, cangjieTable, expectedNumOfEntry,
+		"result table size '%d' not as expected '%d'",
+		len(cangjieTable), expectedNumOfEntry)
 }
 
 func TestReadBuiltinTable(t *testing.T) {
+	const expectedNumOfEntry = 75012
+
 	cangjieTable, err := data.ReadBuiltinTable()
 	if !assert.NoError(t, err) {
 		assert.FailNow(t, "failed parsing table data", err)
 	}
-	assert.Len(t, cangjieTable, 75017)
+	assert.Len(t, cangjieTable, expectedNumOfEntry,
+		"result table size '%d' not as expected '%d'",
+		len(cangjieTable), expectedNumOfEntry)
 }
