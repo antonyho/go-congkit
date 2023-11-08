c51193087021b016b8393216f6fb3bdc8a523520
Antony Ho <ntonyworkshop@gmail.com>
2023-11-08 21:15:32 +0100

    More implementation and unit tests to the engine

--- engine/engine.go
+++ engine/engine.go
@@ -1,5 +1,98 @@
 package engine
 
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type CangjieVersion int
+
+const (
+	CangjieV3 CangjieVersion = 3
+	CangjieV5 CangjieVersion = 5
+)
+
 type Option func(*Engine)
 
-type Engine struct{}
+func WithCangjieV3() Option {
+	return func(e *Engine) {
+		e.CangjieVersion = CangjieV3
+	}
+}
+
+func WithCangjieV5() Option {
+	return func(e *Engine) {
+		e.CangjieVersion = CangjieV5
+	}
+}
+
+func WithSimplified() Option {
+	return func(e *Engine) {
+		e.OutputSimplified = true
+	}
+}
+
+func WithEasy() Option {
+	return func(e *Engine) {
+		e.Easy = true
+	}
+}
+
+func WithPrediction() Option {
+	return func(e *Engine) {
+		e.Prediction = true
+	}
+}
+
+func WithDatabase(path string) Option {
+	return func(e *Engine) {
+		e.dbPath = path
+	}
+}
+
+// Engine defaults
+const (
+	DefaultCangjieVersion = CangjieV5
+	DefaultDatabasePath   = "./cangjie.db"
+)
+
+type Engine struct {
+	CangjieVersion
+	OutputSimplified bool // Output Simplified Chinese word
+	Easy             bool // "Easy" input method mode
+	Prediction       bool // Predict word with given radicals
+	dbPath           string
+	db               *sql.DB
+}
+
+func New(options ...Option) (*Engine, error) {
+	e := &Engine{
+		CangjieVersion:   DefaultCangjieVersion,
+		OutputSimplified: false,
+		dbPath:           DefaultDatabasePath,
+	}
+
+	for _, option := range options {
+		option(e)
+	}
+
+	db, err := sql.Open("sqlite3", e.dbPath)
+	if err != nil {
+		return nil, err
+	}
+	e.db = db
+
+	return e, nil
+}
+
+func (e *Engine) Set(options ...Option) {
+	for _, option := range options {
+		option(e)
+	}
+}
+
+func (e *Engine) Compose(radicals string) (results []rune, err error) {
+	results = make([]rune, 0)
+	return
+}
--- engine/engine_test.go
+++ engine/engine_test.go
@@ -3,6 +3,7 @@ package engine
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -11,9 +12,38 @@ type CangjieV3TestSuite struct {
 	Engine
 }
 
-func (s *CangjieV3TestSuite) SetupTest() {}
+func (s *CangjieV3TestSuite) SetupTest() {
+	engine, err := New(WithCangjieV3())
+	if err != nil {
+		s.FailNow("failed initialising the engine", err)
+	}
+	s.Engine = *engine
+}
 
-func (s *CangjieV3TestSuite) TestEngine_Get() {}
+func (s *CangjieV3TestSuite) TestEngine_Compose() {
+	var testCases = []struct {
+		name      string
+		radicals  string
+		expected  []rune
+		expectErr bool
+	}{
+		{"no match", "", []rune{}, false},
+		{"single match", "", []rune{}, false},
+		{"multiple matches", "", []rune{}, false},
+		{"punctuation", "", []rune{}, false},
+		{"db error", "", []rune{}, true},
+	}
+
+	for _, testCase := range testCases {
+		s.T().Run(testCase.name, func(t *testing.T) {
+			results, err := s.Engine.Compose(testCase.radicals)
+			if testCase.expectErr {
+				assert.Error(s.T(), err)
+			}
+			assert.EqualValues(s.T(), testCase.expected, results)
+		})
+	}
+}
 
 func TestCangjieV3TestSuite(t *testing.T) {
 	suite.Run(t, new(CangjieV3TestSuite))
@@ -24,10 +54,73 @@ type CangjieV5TestSuite struct {
 	Engine
 }
 
-func (s *CangjieV5TestSuite) SetupTest() {}
+func (s *CangjieV5TestSuite) SetupTest() {
+	engine, err := New(WithCangjieV5())
+	if err != nil {
+		s.FailNow("failed initialising the engine", err)
+	}
+	s.Engine = *engine
+}
 
-func (s *CangjieV5TestSuite) TestEngine_Get() {}
+func (s *CangjieV5TestSuite) TestEngine_Compose() {}
 
 func TestCangjieV5TestSuite(t *testing.T) {
 	suite.Run(t, new(CangjieV5TestSuite))
+}
+
+type WithSimplifiedTestSuite struct {
+	suite.Suite
+	Engine
+}
+
+func (s *WithSimplifiedTestSuite) SetupTest() {
+	engine, err := New(WithSimplified())
+	if err != nil {
+		s.FailNow("failed initialising the engine", err)
+	}
+	s.Engine = *engine
+}
+
+func (s *WithSimplifiedTestSuite) TestEngine_Compose() {}
+
+func TestWithSimplifiedTestSuite(t *testing.T) {
+	suite.Run(t, new(WithSimplifiedTestSuite))
+}
+
+type WithEasyTestSuite struct {
+	suite.Suite
+	Engine
+}
+
+func (s *WithEasyTestSuite) SetupTest() {
+	engine, err := New(WithEasy())
+	if err != nil {
+		s.FailNow("failed initialising the engine", err)
+	}
+	s.Engine = *engine
+}
+
+func (s *WithEasyTestSuite) TestEngine_Compose() {}
+
+func TestWithEasyTestSuite(t *testing.T) {
+	suite.Run(t, new(WithEasyTestSuite))
+}
+
+type WithPredictionTestSuite struct {
+	suite.Suite
+	Engine
+}
+
+func (s *WithPredictionTestSuite) SetupTest() {
+	engine, err := New(WithPrediction())
+	if err != nil {
+		s.FailNow("failed initialising the engine", err)
+	}
+	s.Engine = *engine
+}
+
+func (s *WithPredictionTestSuite) TestEngine_Compose() {}
+
+func TestWithPredictionTestSuite(t *testing.T) {
+	suite.Run(t, new(WithPredictionTestSuite))
 }
